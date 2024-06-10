266416e19ae47dc4570d1a66ce04e5a748a3b797
Alex <131786787+Spelks@users.noreply.github.com>
2024-06-10 09:43:36 +0100

    Create Book constructor and function to send to library array.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
+const bookForm = document.querySelector("[data-book-form]");
+
+bookForm.addEventListener("submit", addBookToLibrary);
 
 // Initial "Add Book" button opens the dialog modally
 showButton.addEventListener("click", () => {
@@ -14,10 +17,18 @@ closeButton.addEventListener("click", () => {
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const title = document.querySelector("[data-title]").value;
+  const author = document.querySelector("[data-author]").value;
+  const pages = document.querySelector("[data-pages]").value;
+  dialog.close();
+  bookForm.reset();
+  myLibrary.push(new Book(title, author, pages));
 }
