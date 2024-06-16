96276ca6c171c9b99c0366a22ff36c1873c6d9db
Alex <131786787+Spelks@users.noreply.github.com>
2024-06-16 20:55:17 +0100

    Move variables into forEach. Add function to remove book div and matching array item.

--- script.js
+++ script.js
@@ -51,11 +51,12 @@ function addBookToLibrary(event) {
 
 //Creates the visual representation of the book and applies a data attribute
 function showBookOnShelf() {
-  const newDiv = document.createElement("div");
-  const deleteBtn = document.createElement("button");
-  const newP = document.createElement("p");
-  const authorP = document.createElement("p");
-  myLibrary.forEach(book => {
+  shelf.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    const newDiv = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+    const newP = document.createElement("p");
+    const authorP = document.createElement("p");
     shelf.appendChild(newDiv);
     newDiv.appendChild(deleteBtn);
     deleteBtn.textContent = "X";
@@ -64,6 +65,16 @@ function showBookOnShelf() {
     newP.textContent = book.title;
     authorP.textContent = book.author;
     newDiv.classList.add("book");
-    newDiv.setAttribute("data-id", myLibrary.length -1);  //Data attibute testing  
+    newDiv.setAttribute("data-id", index);
+    deleteBtn.addEventListener("click", ()=> {
+      deleteBook(newDiv);
+    });
   });
+}
+
+//Removes book div (and all children) matching library array object then runs book function to refresh 
+function deleteBook(book) {
+  shelf.removeChild(book);
+  myLibrary.splice(book.getAttribute("data-id"), 1);
+  showBookOnShelf();
 }
